519c287cc9a4a6cc3e5b0ae2595c10a6848f0243
taolipeng <taolipeng@zuber.im>
2017-09-04 19:34:29 +0800

    项目

---
+++ WeProject/src/main/java/com/shareTools/Test/BookDaoTest.java
@@ -0,0 +1,36 @@
+package com.shareTools.Test;
+
+import com.shareTools.dao.BookDao;
+import com.shareTools.entity.Book;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class BookDaoTest {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Test
+    public void testQueryById() throws Exception {
+        long bookId = 1000;
+        Book book = bookDao.queryById(bookId);
+        System.out.println(book);
+    }
+
+    @Test
+    public void testQueryAll() throws Exception {
+        List<Book> books = bookDao.queryAll(0, 4);
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    @Test
+    public void testReduceNumber() throws Exception {
+        long bookId = 1000;
+        int update = bookDao.reduceNumber(bookId);
+        System.out.println("update=" + update);
+    }
+}
---
+++ WeProject/src/main/java/com/shareTools/service/impl/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.shareTools.service.impl;
+
+public class UserServiceImpl {
+}
