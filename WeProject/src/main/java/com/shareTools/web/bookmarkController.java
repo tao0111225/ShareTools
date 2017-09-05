ed062f8b9f5d7e201270c7c03f2b495a5497fe49
taolipeng <taolipeng@zuber.im>
2017-09-05 18:12:40 +0800

    制定文档 近期任务

--- WeProject/src/main/java/com/shareTools/web/bookmarkController.java
+++ WeProject/src/main/java/com/shareTools/web/bookmarkController.java
@@ -1,4 +1,24 @@
 package com.shareTools.web;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bookmark")
 public class bookmarkController {
+
+
+    /**
+     *  描述：上传 bookmark.html
+     *        使用 js 把 数据源 处理 有层级 json
+     *          使用自己制定算法，写list tree
+     *          传递到后台，进行存储，
+     *          转换 成为 tree 给 前台
+     */
+
+
+
+
+
 }
